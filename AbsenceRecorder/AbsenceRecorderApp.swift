b7439d0aa95a97a43b28e3b981f9ea82150e2934
freddien03 <60051731+freddien03@users.noreply.github.com>
2021-03-10 16:42:00 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Freddie Nunn on 10/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
